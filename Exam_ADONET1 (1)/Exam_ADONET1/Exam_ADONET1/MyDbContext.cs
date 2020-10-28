da7bca9885b6ca521bba921d0ffd77db79668f9a
bryzgaalov <49521831+bryzgaalov@users.noreply.github.com>
2020-10-28 12:40:54 +0600

    Add files via upload

---
+++ Exam_ADONET1 (1)/Exam_ADONET1/Exam_ADONET1/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_ADONET_1
+{
+
+    class MyDbContext : DbContext
+    {
+
+        public MyDbContext() : base("SqlConnection")
+        {
+
+        }
+        public DbSet<xDataDoc> dataDocs { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+}
